cff8c17ac85fa11e8d941932670499278cb3eb7c
olegkushnarev <styx.inc4@gmail.com>
2022-11-29 09:41:16 +0700

    CatBoost

---
+++ Kushnarev/cat_boost_full.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import argparse
+from pathlib import Path
+from catboost import CatBoostRegressor as Cat
+from joblib import dump
+
+BEST_PARAMETERS = {'max_depth': 3, 'n_estimators': 50, 'silent': True}
+
+
+def parser_args_for_sac():
+    parser = argparse.ArgumentParser(description='Paths parser')
+    parser.add_argument('--input_dir', '-id', type=str, default='data/prepared/',
+                        required=False, help='path to input data directory')
+    parser.add_argument('--output_dir', '-od', type=str, default='data/models/',
+                        required=False, help='path to save prepared data')
+    parser.add_argument('--model_name', '-mn', type=str, default='LR', required=False,
+                        help='file with dvc stage params')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parser_args_for_sac()
+
+    input_dir = Path(args.input_dir)
+    output_dir = Path(args.output_dir)
+
+    output_dir.mkdir(exist_ok=True, parents=True)
+    output_model_path = output_dir / (args.model_name + '_prod.jpg')
+    output_model_joblib_path = output_dir / (args.model_name + '_prod.joblib')
+
+    X_train_name = input_dir / 'X_full.csv'
+    y_train_name = input_dir / 'y_full.csv'
+
+    X_train = pd.read_csv(X_train_name)
+    y_train = pd.read_csv(y_train_name)
+    y_train_cols = y_train.columns
+
+    reg = Cat(**BEST_PARAMETERS)
+
+    dump(reg, output_model_joblib_path)
